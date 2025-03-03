1f0efc0a1b3c40d8935ce37e10c468cc3da1cde7
Suprateek Sen <suprateeksen62@gmail.com>
2025-03-03 22:37:20 +0530

    done

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:5000/api"
+export const baseUrl = import.meta.env.URL
 
 export const postRequest = async (url,body)=>{
         const respoce = await fetch(url, {
